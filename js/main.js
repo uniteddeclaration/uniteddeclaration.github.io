e055b704f3ae0e2b23a2fa3f24db2f3bc410040d
universaldeclaration <universaldeclaration@outlook.com>
2021-08-09 19:40:07 +0800

    Revert "main"
    This reverts commit 6bf88b8f2f6fb2a7a6ae6e701ec3f4a956d67ae6.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("a").attr({
         title: "all titles are the same too!",
-        href: ""
+        href: "index.html"
     });
     console.log($("a").attr("href"));
 });
